01d2eea93f5259ed4f6036e7f8c44ce4a1ba9a96
Andy Gorman <hi@gorman.zone>
2023-09-04 17:40:31 -0400

    Move off vim, create neovims setup files and script

---
+++ aspects/nvim/.config/nvim/after/plugin/nerdtree.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', '<c-b>', ':NERDTreeToggle<CR>', {})
+vim.g.NERDTreeWinSize = 40
---
+++ aspects/nvim/.config/nvim/after/plugin/projectionist.lua
@@ -0,0 +1,16 @@
+vim.g.projectionist_heuristics = {
+	['*'] = {
+    ['*.ts'] = {
+ 			['alternate'] = '{}.hbs', 
+      ['type'] = 'controller',
+    },
+    ['*.js'] = {
+			['alternate'] = '{}.hbs', 
+      ['type'] = 'controller',
+    },
+    ['*.hbs'] = {
+ 	    ['alternate'] = {'{}.ts', '{}.js'},
+ 			['type'] = 'template'
+    }
+  }
+}
