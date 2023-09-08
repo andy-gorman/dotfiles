b30c218952ae1c8790d8cb554450267154763689
Andy Gorman <hi@gorman.zone>
2023-09-08 18:07:56 -0400

    Run stylua

--- aspects/nvim/.config/nvim/after/plugin/nerdtree.lua
+++ aspects/nvim/.config/nvim/after/plugin/nerdtree.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap('n', '<c-b>', ':NERDTreeToggle<CR>', {})
+vim.api.nvim_set_keymap("n", "<c-b>", ":NERDTreeToggle<CR>", {})
 vim.g.NERDTreeWinSize = 40
--- aspects/nvim/.config/nvim/init.lua
+++ aspects/nvim/.config/nvim/init.lua
@@ -7,45 +7,45 @@ vim.opt.ignorecase = true -- Search case insensitive...
 vim.opt.smartcase = true -- ... but not when search patterns contains upper case characters
 vim.opt.showmatch = true -- highlight matching brace/paren/etc.
 -- Folding options
-vim.opt.foldmethod='indent'
-vim.opt.foldlevelstart=1 -- Probably change this at some point, but this is teaching me to use folds
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 1 -- Probably change this at some point, but this is teaching me to use folds
 
 -- Tab settings
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
-vim.opt.smartindent = true 
+vim.opt.smartindent = true
 
 vim.opt.fixeol = true -- Dont add <EOL> at end of file
 
 -- use mouse
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- autocomplete options
-vim.opt.wildmode = 'longest:full,full'
-vim.opt.wildignore = vim.opt.wildignore + '*/build/*,*/node_modules/*'
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore = vim.opt.wildignore + "*/build/*,*/node_modules/*"
 
 -- Copy / paste
-clipboard=unnamed
+clipboard = unnamed
 
 -- Escape Replacement
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {})
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", {})
 
 -- Move splits
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-W>h', {})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l', {})
-vim.api.nvim_set_keymap('n', '<C-J>', '<C-W>j', {})
-vim.api.nvim_set_keymap('n', '<C-K>', '<C-W>k', {})
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-W>h", {})
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-W>l", {})
+vim.api.nvim_set_keymap("n", "<C-J>", "<C-W>j", {})
+vim.api.nvim_set_keymap("n", "<C-K>", "<C-W>k", {})
 
 -- Toggle fold at current position.
-vim.api.nvim_set_keymap('', '<s-tab>', 'za', {noremap = true})
+vim.api.nvim_set_keymap("", "<s-tab>", "za", { noremap = true })
 
-vim.api.nvim_create_autocmd('FocusLost', { pattern = "*", command = ':wa' })		-- Set vim to save the file on focus out.
+vim.api.nvim_create_autocmd("FocusLost", { pattern = "*", command = ":wa" }) -- Set vim to save the file on focus out.
 
 -- Colors
 local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
 local is_set_theme_file_readable = vim.fn.filereadable(vim.fn.expand(set_theme_path)) == 1 and true or false
 if is_set_theme_file_readable then
-	vim.cmd('let base16colorspace=256')
-  vim.cmd('source' .. set_theme_path)
+	vim.cmd("let base16colorspace=256")
+	vim.cmd("source" .. set_theme_path)
 end
