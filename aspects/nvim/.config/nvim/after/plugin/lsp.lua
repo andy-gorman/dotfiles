6b21bf69295fa8942002b0b213b3e3e0fdd9b98a
Andy Gorman <hi@gorman.zone>
2023-09-15 21:49:26 -0400

    lint'

--- aspects/nvim/.config/nvim/after/plugin/lsp.lua
+++ aspects/nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,8 +1,8 @@
 local lspconfig = require("lspconfig")
 
-local on_attach = function() 
-	vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = true, silent = true })
+local on_attach = function()
+	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true, silent = true })
 end
 
-lspconfig.tsserver.setup({on_attach = on_attach})
-lspconfig.glint.setup({on_attach = on_attach})
+lspconfig.tsserver.setup({ on_attach = on_attach })
+lspconfig.glint.setup({ on_attach = on_attach })
