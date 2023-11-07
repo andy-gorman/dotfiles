7a8102d95e63b142cc504239ceabca49a39e7e4c
Andy Gorman <hi@gorman.zone>
2023-11-07 14:26:12 -0500

    Add diagnostic vim mappings

--- aspects/nvim/.config/nvim/after/plugin/lsp.lua
+++ aspects/nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,11 @@
 local lspconfig = require("lspconfig")
 
+-- Global mappings
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
 local on_attach = function()
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = true, silent = true })
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = true })
@@ -7,3 +13,4 @@ end
 
 lspconfig.tsserver.setup({ on_attach = on_attach })
 lspconfig.glint.setup({ on_attach = on_attach })
+lspconfig.lua_ls.setup({ on_attach = on_attach})
