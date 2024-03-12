b1327237de82ab2b2ac3e985e621b2d8b0184357
Andy Gorman <hi@gorman.zone>
2024-03-12 14:16:32 -0400

    Add controller/route/template mappings

--- aspects/nvim/.config/nvim/after/plugin/projectionist.lua
+++ aspects/nvim/.config/nvim/after/plugin/projectionist.lua
@@ -12,5 +12,25 @@ vim.g.projectionist_heuristics = {
 			["alternate"] = { "{}.ts", "{}.js" },
 			["type"] = "template",
 		},
+		["controller.js"] = {
+			["alternate"] = "template.hbs",
+			["type"] = "controller"
+		},
+		["controller.ts"] = {
+			["alternate"] = "template.hbs",
+			["type"] = "controller"
+		},
+		["template.hbs"] = {
+			["alternate"] = { "route.ts", "route.js" },
+			["type"] = "template"
+		},
+		["route.ts"] = {
+			["alternate"] = { "controller.ts", "controller.js" },
+			["type"] = "route"
+		},
+		["route.js"] = {
+			["alternate"] = { "controller.ts", "controller.js" },
+			["type"] = "route"
+		},
 	},
 }
