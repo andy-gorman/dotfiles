5862657e09dbcea662725b2e73c3fa5d4019467b
Andy Gorman <hi@gorman.zone>
2025-02-17 14:12:18 -0500

    Respect LSP

--- aspects/nvim/.config/nvim/after/plugin/lsp.lua
+++ aspects/nvim/.config/nvim/after/plugin/lsp.lua
@@ -25,7 +25,7 @@ lspconfig.ts_ls.setup({
 })
 lspconfig.glint.setup({ on_attach = on_attach })
 lspconfig.eslint.setup({
-	on_attach = function(client, bufnr)
+	on_attach = function(_, bufnr)
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			buffer = bufnr,
 			command = "EslintFixAll",
