64b38f17f4f0b4b7b136b55165d6eed94d19ea5b
Andy Gorman <hi@gorman.zone>
2025-12-30 15:15:34 -0500

    Use base24_tomorrow theme for now

--- aspects/dotfiles/files/.bashrc
+++ aspects/dotfiles/files/.bashrc
@@ -52,12 +52,9 @@ export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
 # Let's be normal
 set -o emacs
 
-# Base16 Shell
-BASE16_SHELL_PATH="$HOME/.config/base16-shell/"
-[ -n "$PS1" ] && \
-    [ -s "$BASE16_SHELL_PATH/profile_helper.sh" ] && \
-        source "$BASE16_SHELL_PATH/profile_helper.sh"
-base16_gruvbox-material-dark-soft
+# Tinted Shell
+TINTED_SHELL_ENABLE_BASE24_VARS=1
+. $HOME/.config/base16-shell/scripts/base24-tomorrow-night.sh
 
 
 ## NVM
--- aspects/nvim/.config/nvim/after/plugin/colors.lua
+++ aspects/nvim/.config/nvim/after/plugin/colors.lua
@@ -1,8 +1,17 @@
 vim.opt.termguicolors = true
 
--- Set colorscheme based on BASE16_THEME environment variable
-local base16_theme = vim.env.BASE16_THEME or "nord"
-local colorscheme = "base16-" .. base16_theme
+local base24_theme = vim.env.BASE24_THEME
+local base16_theme = vim.env.BASE16_THEME
+
+local colorscheme
+if base24_theme and base24_theme ~= "" then
+	colorscheme = "base24-" .. base24_theme
+elseif base16_theme and base16_theme ~= "" then
+	colorscheme = "base16-" .. base16_theme
+else
+	vim.notify("No BASE24_THEME or BASE16_THEME set, skipping colorscheme", vim.log.levels.WARN)
+	return
+end
 
 require("tinted-colorscheme").setup(colorscheme, {
 	supports = {
